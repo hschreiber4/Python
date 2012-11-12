ac2d6265468823024d17be71eb137dbf31c9d550
hschreiber4 <henrylschreiber@gmail.com>
2012-11-12 17:29:50 -0600

    Continued changes, new job

---
+++ Learning_input.py
@@ -0,0 +1,9 @@
+import sys
+
+print "Activated"
+
+input1 = sys.argv[1]
+input2 = sys.argv[2]
+
+print 'The number 1 is spelled: '+ input1
+print 'The number 2 is spelled: '+ input2
---
+++ microarray_annotation/micronat.py
@@ -0,0 +1,5 @@
+#Hard coding the input file for easier coding to start with
+
+
+for item in open('short_file.txt'):
+	print item
