9d3c4169fb6b4f60775f65ad0a8fdb234c5d9782
hschreiber4 <henrylschreiber@gmail.com>
2012-11-13 10:43:33 -0600

    CDS doesn't work.  REVERT REVERT REVERT

--- microarray_annotation/micronat.py
+++ microarray_annotation/micronat.py
@@ -16,35 +16,36 @@ out_file=open('output.txt','wt')
 for line in open('short_file.txt'):
 
 #Now the searches begin
-	# if re.search('CDS') and re.search('..'):
-	# 	pass
+	if re.search('CDS', line) and re.search('..', line):
 #Finding the gene location and then isolating the numbers
-	if re.search('gene', line) and re.search('\.\.', line):
-		gene_loc = line.split('gene')
+		gene_loc = line.split('CDS')
 		formatted_gene_loc = gene_loc[1].replace(' ','').rstrip('\n')
 		gene_boundaries = formatted_gene_loc.split('..')
 		gene_size=int(gene_boundaries[1])-int(gene_boundaries[0])
 		out_file.write(str(gene_boundaries[0])+'\t'+str(gene_boundaries[1])+'\t'+str(gene_size)+'\t')
 
+		for subline in open('short_file.txt'):
+
 #Finding the gene name and isolating it
-	elif re.search('/gene',line):
-		gene_tag = isolate('/gene=',line)
+			if re.search('/gene',subline):
+				gene_tag = isolate('/gene=',subline)
+				print gene_tag
 
 #Finding the locus tag and isolating it
-	elif re.search('/locus_tag',line):
-		locus_tag = isolate('tag=', line)
+			elif re.search('/locus_tag',subline):
+				locus_tag = isolate('tag=', subline)
 
 #Finding the function and isolating it
-	elif re.search('/function', line):
-		function_tag = isolate('function=', line)
+			elif re.search('/function', subline):
+				function_tag = isolate('function=', subline)
 
 #Finding the protein id and isolating it
-	elif re.search('protein_id=',line):
-		protein_tag = isolate('_id=', line)
+			elif re.search('protein_id=',subline):
+				protein_tag = isolate('_id=', subline)
 
 #Finding the GI and isolating it
-	elif re.search('"GI:',line):
-		GI_tag = isolate('xref=',line)
+			elif re.search('"GI:',subline):
+				GI_tag = isolate('xref=',subline)
 
 
 
