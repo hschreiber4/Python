ce03876c7a600bfb2fe1e6baca3fc6823dac8376
hschreiber4 <henrylschreiber@gmail.com>
2012-11-12 21:30:55 -0600

    Starting to add regex

--- microarray_annotation/micronat.py
+++ microarray_annotation/micronat.py
@@ -1,5 +1,9 @@
+#Here are all my imports
+import re
+import sys
+
 #Hard coding the input file for easier coding to start with
-
-
-for item in open('short_file.txt'):
-	print item
+for line in open('short_file.txt'):
+	item = line.split(' ')
+	for portion in item:
+		print portion
