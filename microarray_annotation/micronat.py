be61f59cb37662efc330cc4bc8d84110097bd0cd
hschreiber4 <henrylschreiber@gmail.com>
2012-11-13 09:54:44 -0600

    Starting Print Statement

--- microarray_annotation/micronat.py
+++ microarray_annotation/micronat.py
@@ -18,10 +18,12 @@ for line in open('short_file.txt'):
 	if re.search('gene', line) and re.search('\.\.', line):
 		gene_loc = line.split('gene')
 		formatted_gene_loc = gene_loc[1].replace(' ','').rstrip('\n')
+		gene_boundaries = formatted_gene_loc.split('..')
+		gene_size=int(gene_boundaries[1])-int(gene_boundaries[0])
 
 #Finding the gene name and isolating it
 	elif re.search('/gene',line):
-		gene_name = isolate('/gene=',line)
+		gene_tag = isolate('/gene=',line)
 
 #Finding the locus tag and isolating it
 	elif re.search('/locus_tag',line):
@@ -33,17 +35,22 @@ for line in open('short_file.txt'):
 
 #Finding the protein id and isolating it
 	elif re.search('protein_id=',line):
-		protein_name = isolate('_id=', line)
+		protein_tag = isolate('_id=', line)
 
 #Finding the GI and isolating it
 	elif re.search('"GI:',line):
 		GI_tag = isolate('xref=',line)
 
-#Finding the note and isolating it
-	elif re.search('/note=',line):
-		note_tag = isolate('/note=',line)
-		while True:
-			note_lines=line.readline()
-			if re.match([a-z],note_lines):
-				print note_lines
-			elif re.match()
+#Making a print statement
+	# print '{gene_start}\t{gene_end}\t{gene_length}\t{gene_name}\t{locus_name}\t{function_name}\t{protein_name}\t{GI_name}'.format(gene_start=gene_boundaries[0], gene_end=gene_boundaries[1], gene_length=gene_size, gene_name=gene_size, locus_name=locus_tag, function_name=function_tag, protein_name=protein_tag, GI_name=GI_tag)
+
+
+# #Finding the note and isolating it
+# 	elif re.search('/note=',line):
+# 		note_tag = isolate('/note=',line)
+# 		while True:
+# 			note_lines=line.read()
+# 			if re.search('/codon_start',note_lines):
+# 				break
+# 			else:
+# 				print note_lines
