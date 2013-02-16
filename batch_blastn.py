785888f4fa7334bee81981b536b6a0cd00775615
hschreiber4 <henrylschreiber@gmail.com>
2013-02-15 20:47:32 -0600

    working

--- batch_blastn.py
+++ batch_blastn.py
@@ -17,31 +17,48 @@ if len(sys.argv)!=3:
 #This section loads the files.
 files = os.listdir(sys.argv[1])
 
-#Starting the loop to look for files.
-for filename in files:
-	if filename.endswith('.fa'):
-		print 'Working on '+filename.rstrip('.fa')
+#This counter and list will be used in the construction of the presence/absence table for the genes
+strain_list=['11.1a', '21.1a', '35.3r', '9.2p', '11.2p', '21.2p', '41.1a', '9.3r', '11.3r', '21.3r', '41.2p', 'ABU', '12.1a', '2.1a', '41.3r', 'APEC', '12.2p', '2.2r', '41.4p', 'CFT073', '12.3r', '26.1a', '41.5r', 'E24377A', '12.4r', '26.2p', '5.1a', 'IAI39', '13.2p', '26.3r', '5.2p', 'MG1655', '13.4p', '31.1a', '536', 'O157', '17.1a', '31.2p', '5.3r', 'S88', '17.2p', '31.3r', '55989', 'UTI89', '17.3r', '34.1a', '56.1r', 'UTI89', '20.1a', '34.2r', '56.2r', '20.2r', '35.1a', '56.3r', '20.3r', '35.2p', '9.1a']
 
-		# #Setting the blastn command
-		# blastn_cmd='blastn -db UPEC_TOP_ORFS_db -query '+sys.argv[1]+'/'+filename+' -outfmt "6 sseqid evalue bitscore pident qlen slen qstart qend sstart send sseq">'+sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt'
-		# subprocess.call(blastn_cmd, shell=True)
-
-		#Ediitng the file to take out the spurious results
-		if os.path.exists(sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt')==True:
-			print 'blastn results file created for'+filename.rstrip('.fa')
-			infile = open (sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt')
-			outfile = open (sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results_edited.txt', 'wt')
+#The results file will contain data on the presence or absence of genes from the genomes
+results_file = open ('Restults.txt','wt')
+results_file.write('StrainIDs')
+for strain in strain_list:
+	results_file.write('\t'+strain)
 
 
-			for line in infile:
-				line_parts = line.split('\t')
-				print 'Query Length for '+line_parts[0].split('_')[0]+' is '+line_parts[5]
-				print 'Subject Length for '+line_parts[0].split('_')[0]+' is '+line_parts[4]
-				limit= int(line_parts[4])+1000
-				if int(line_parts[5])<int(limit):
-					outfile.write(line))
-					
-		#Killing the program if it doesn't make the file.
-		if os.path.exists(sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt')==False:
-			print 'There was a problem making the blastn results file for '+filename.rstrip('.fa')
-			sys.exit()
+# #Starting the loop to look for files.
+# for filename in files:
+# 	if filename.endswith('.fa'):
+# 		print 'Working on '+filename.rstrip('.fa')
+
+# 		# # #Setting the blastn command
+# 		# blastn_cmd='blastn -db UPEC_TOP_ORFS_db -query '+sys.argv[1]+'/'+filename+' -outfmt "6 sseqid evalue bitscore pident qlen slen qstart qend sstart send sseq">'+sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt'
+# 		# subprocess.call(blastn_cmd, shell=True)
+
+# 		# #Ediitng the file to take out the spurious results
+# 		# if os.path.exists(sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt')==True:
+# 		# 	print 'blastn results file created for '+filename.rstrip('.fa')
+# 		# 	infile = open (sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt')
+# 		# 	outfile = open (sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results_edited.txt', 'wt')
+			
+# 		# 	#Let's write some headers!
+# 		# 	outfile.write('SubjectID\tEvalue\tBitscore\tPercentID\tQueryLength\tSubjectLength\tQueryStart\tQueryEnd\tSubjectStart\tSubjectEnd\tSubjectSeq\n')
+
+# 		# 	#This section will remove hits from ORFs that are not meant to be there in the first place, due to glimmer's idiosyncracies.
+# 		# 	for line in infile:
+# 		# 		line_parts = line.split('\t')
+# 		# 		limit= int(line_parts[4])*2
+# 		# 		if int(line_parts[5])<int(limit):
+# 		# 			strain_list.append(line_parts[0].split('_')[0])
+# 		# 			outfile.write(line)
+
+			
+# 		# 	print 'blastn results file edited for '+filename.rstrip('.fa')
+# 		# 	infile.close()
+
+
+# 		# #Killing the program if it doesn't make the file.
+# 		# if os.path.exists(sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt')==False:
+# 		# 	print 'There was a problem making the blastn results file for '+filename.rstrip('.fa')
+# 		# 	sys.exit()
--- rename.py
+++ rename.py
@@ -1,17 +1,31 @@
 import sys
 import os
-import re
 
-# translate={"SJH_15_ORFS.fa":"2.1a.fa", "SJH_16_ORFS.fa":"2.2r.fa", "SJH_17_ORFS.fa":"5.1a.fa", "SJH_171_ORFS.fa":"5.2p.fa", "SJH_18_ORFS.fa":"5.3r.fa", "SJH_19_ORFS.fa":"9.1a.fa", "SJH_173_ORFS.fa":"9.2p.fa", "SJH_20_ORFS.fa":"9.3r.fa", "SJH_21_ORFS.fa":"11.1a.fa", "SJH_175_ORFS.fa":"11.2p.fa", "SJH_22_ORFS.fa":"11.3r.fa", "SJH_23_ORFS.fa":"12.1a.fa", "SJH_176_ORFS.fa":"12.2p.fa", "SJH_24_ORFS.fa":"12.3r.fa", "SJH_25_ORFS.fa":"12.4r.fa", "SJH_26_ORFS.fa":"13.1a.fa", "SJH_179_ORFS.fa":"13.2p.fa", "SJH_27_ORFS.fa":"13.3r.fa", "SJH_181_ORFS.fa":"13.4p.fa", "SJH_28_ORFS.fa":"13.5r.fa", "SJH_29_ORFS.fa":"17.1a.fa", "SJH_186_ORFS.fa":"17.2p.fa", "SJH_30_ORFS.fa":"17.2r.fa", "SJH_31_ORFS.fa":"20.1a.fa", "SJH_32_ORFS.fa":"20.2r.fa", "SJH_33_ORFS.fa":"20.3r.fa", "SJH_34_ORFS.fa":"21.1a.fa", "SJH_189_ORFS.fa":"21.2p.fa", "SJH_35_ORFS.fa":"21.3r.fa", "SJH_36_ORFS.fa":"26.1a.fa", "SJH_191_ORFS.fa":"26.2p.fa", "SJH_37_ORFS.fa":"26.3r.fa", "SJH_38_ORFS.fa":"31.1a.fa", "SJH_289_ORFS.fa":"31.2p.fa", "SJH_39_ORFS.fa":"31.3r.fa", "SJH_291_ORFS.fa":"34.1a.fa", "SJH_292_ORFS.fa":"34.2r.fa", "SJH_294_ORFS.fa":"35.1a.fa", "SJH_296_ORFS.fa":"35.2p.fa", "SJH_295_ORFS.fa":"35.3r.fa", "SJH_298_ORFS.fa":"41.1a.fa", "SJH_300_ORFS.fa":"41.2p.fa", "SJH_299_ORFS.fa":"41.3r.fa", "SJH_303_ORFS.fa":"41.4p.fa", "SJH_302_ORFS.fa":"41.5r.fa", "SJH_338_ORFS.fa":"56.1r.fa", "SJH_341_ORFS.fa":"56.2r.fa", "SJH_345_ORFS.fa":"56.3r"}
+if len(sys.argv)!=2:
+	print '\n'+'\n'+'\n'+'To use this script, type: python batch_blastn.py {directory of query sequences[USE THE FULL PATH WITH NO END SLASH]} {output directory [USE THE FULL PATH AND NO END SLASH}'+'\n'
+	if len(sys.argv)<3:
+		print 'You forgot something!'+'\n'
+	elif len(sys.argv)>3:
+		print 'You added something!'+'\n'
+	sys.exit()
 
-os.rename('./SJH_15_ORFS','./2.1a.fa')
+file_list=os.listdir(sys.argv[1])
 
-# files = os.listdir('/Users/hschreiber4/Dropbox/Hultgren_Lab_Research/predicted_orfs')
+for item in file_list:
+	if item.endswith('.fa'):
+		infile = open (item)
+		outfile = open (item.rstrip('.fa')+'_formatted.fa', 'wt')
+		for line in infile:
+			if line.startswith('>'):
+				line = '>'+item.rstrip('.fa')+'_'+line.lstrip('>')
+				outfile.write(line)
+			else:
+				outfile.write(line)
+# translate={'SJH_15.fa':'2.1a.fa', 'SJH_16.fa':'2.2r.fa', 'SJH_17.fa':'5.1a.fa', 'SJH_171.fa':'5.2p.fa', 'SJH_18.fa':'5.3r.fa', 'SJH_19.fa':'9.1a.fa', 'SJH_173.fa':'9.2p.fa', 'SJH_20.fa':'9.3r.fa', 'SJH_21.fa':'11.1a.fa', 'SJH_175.fa':'11.2p.fa', 'SJH_22.fa':'11.3r.fa', 'SJH_23.fa':'12.1a.fa', 'SJH_176.fa':'12.2p.fa', 'SJH_24.fa':'12.3r.fa', 'SJH_25.fa':'12.4r.fa', 'SJH_26.fa':'13.1a.fa', 'SJH_179.fa':'13.2p.fa', 'SJH_27.fa':'13.3r.fa', 'SJH_181.fa':'13.4p.fa', 'SJH_28.fa':'13.5r.fa', 'SJH_29.fa':'17.1a.fa', 'SJH_186.fa':'17.2p.fa', 'SJH_30.fa':'17.2r.fa', 'SJH_31.fa':'20.1a.fa', 'SJH_32.fa':'20.2r.fa', 'SJH_33.fa':'20.3r.fa', 'SJH_34.fa':'21.1a.fa', 'SJH_189.fa':'21.2p.fa', 'SJH_35.fa':'21.3r.fa', 'SJH_36.fa':'26.1a.fa', 'SJH_191.fa':'26.2p.fa', 'SJH_37.fa':'26.3r.fa', 'SJH_38.fa':'31.1a.fa', 'SJH_289.fa':'31.2p.fa', 'SJH_39.fa':'31.3r.fa', 'SJH_291.fa':'34.1a.fa', 'SJH_292.fa':'34.2r.fa', 'SJH_294.fa':'35.1a.fa', 'SJH_296.fa':'35.2p.fa', 'SJH_295.fa':'35.3r.fa', 'SJH_298.fa':'41.1a.fa', 'SJH_300.fa':'41.2p.fa', 'SJH_299.fa':'41.3r.fa', 'SJH_303.fa':'41.4p.fa', 'SJH_302.fa':'41.5r.fa', 'SJH_338.fa':'56.1r.fa', 'SJH_341.fa':'56.2r.fa', 'SJH_345.fa':'56.3r'}
 
 
-# for filename in files:
-# 	if filename.endswith('SJH_15_ORFS.fa'):
-# 		print 'Working on '+filename.rstrip('.fa')
-# 		# newname = translate[filename]
-# 		newname='2.1a.fa'
-# 		os.rename(filename,newname)
+# for item in file_list:
+# 	if ite
+# 	print 'The '+key+' should be '+translate[key]
+# 	os.rename(key, translate[key])
+
