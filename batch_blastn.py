89858eea1ced399eb4696b7dd9356941e5b61087
hschreiber4 <henrylschreiber@gmail.com>
2013-02-15 23:50:40 -0600

    NEVER ENDING LOOP!

--- batch_blastn.py
+++ batch_blastn.py
@@ -18,44 +18,53 @@ if len(sys.argv)!=3:
 files = os.listdir(sys.argv[1])
 
 #This counter and list will be used in the construction of the presence/absence table for the genes
-strain_list=['11.1a', '21.1a', '35.3r', '9.2p', '11.2p', '21.2p', '41.1a', '9.3r', '11.3r', '21.3r', '41.2p', 'ABU', '12.1a', '2.1a', '41.3r', 'APEC', '12.2p', '2.2r', '41.4p', 'CFT073', '12.3r', '26.1a', '41.5r', 'E24377A', '12.4r', '26.2p', '5.1a', 'IAI39', '13.2p', '26.3r', '5.2p', 'MG1655', '13.4p', '31.1a', '536', 'O157', '17.1a', '31.2p', '5.3r', 'S88', '17.2p', '31.3r', '55989', 'UTI89', '17.3r', '34.1a', '56.1r', 'UTI89', '20.1a', '34.2r', '56.2r', '20.2r', '35.1a', '56.3r', '20.3r', '35.2p', '9.1a']
+strain_list=['UTI89-NCBI', 'UTI89', 'S88-NCBI', 'APECO1-NCBI', '5.3r', '5.2p', '34.2r', '34.1', '31.3r', '536-NCBI', '35.3r', '35.2p', '35.1', '21.3r', '21.2p', '21.1', '20.3r', '20.2r', '20.1', '41.3r', '41.2p', '41.1', 'CFT073-NCBI', 'ABU83972-NCBI', '56.3r', '56.2r', '17.3r', '17.2p', '17.1', '26.3r', '26.2p', '26.1', '13.4p', '13.2p', '12.4r', '12.3r', '12.2p', '12.1', 'E24377A-NCBI', '9.3r', '9.2p', '56.1r', '5.1', '41.5r', '41.4p', '31.2p', '55989-NCBI', 'EC4115-NCBI', 'MG1655-NCBI', '11.3r', '11.2p', '11.1a.fa', '31.1', '9.1', '2.2r', 'IAI39-NCBI', '2.1']
+strain_list.sort()
 
 #The results file will contain data on the presence or absence of genes from the genomes
-results_file = open ('Restults.txt','wt')
+results_file = open ('Results.txt','wt')
 results_file.write('StrainIDs')
 for strain in strain_list:
 	results_file.write('\t'+strain)
 
-
 #Starting the loop to look for files.
 for filename in files:
 	if filename.endswith('.fa'):
+		id_list=duplicate_checklist=[]
 		print 'Working on '+filename.rstrip('.fa')
 
 		# #Setting the blastn command
-		blastn_cmd='blastn -db UPEC_TOP_ORFS_db -query '+sys.argv[1]+'/'+filename+' -outfmt "6 sseqid evalue bitscore pident qlen slen qstart qend sstart send sseq">'+sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt'
-		subprocess.call(blastn_cmd, shell=True)
+		# blastn_cmd='blastn -db UPEC_TOP_Genomes_db -query '+sys.argv[1]+'/'+filename+' -outfmt "6 sseqid evalue bitscore pident qlen slen qstart qend sstart send sseq">'+sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt'
+		# subprocess.call(blastn_cmd, shell=True)
 
-		#Ediitng the file to take out the spurious results
+		#Checking to see if file exists
 		if os.path.exists(sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt')==True:
 			print 'blastn results file created for '+filename.rstrip('.fa')
+
+			#Adding the file tested to the Results section
+			results_file.write(filename.rstrip('.fa'))
+			
+			#This section will search the orfs and put the presence or absence in a blastn results file
 			infile = open (sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt')
-			
-
-			#This section will remove hits from ORFs that are not meant to be there in the first place, due to glimmer's idiosyncracies.
 			for line in infile:
-				line_parts = line.split('\t')
-				limit= int(line_parts[4])*2
-				if int(line_parts[5])<int(limit):
-					strain_list.append(line_parts[0].split('_')[0])
-					outfile.write(line)
+				line_parts=line.split('\t')
+				id_parts = line_parts[0].split('_')
+				id_list.append(id_parts[0])
+				id_list.sort()
 
-			
-			print 'blastn results file edited for '+filename.rstrip('.fa')
-			infile.close()
+			print filename.rstrip('.fa')+' is found in the genomes '+str(id_list)
+
+			for name in id_list:
+				# if any(name in x for x in duplicate_checklist):
+				# 	results_file.write('**NEEDS ATTENTION**')
+				if any(name in s for s in strain_list):
+					results_file.write('\t1')
+				else:
+					results_file.write('\t0')
+				duplicate_checklist.append(name)
 
 
-		#Killing the program if it doesn't make the file.
-		if os.path.exists(sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt')==False:
-			print 'There was a problem making the blastn results file for '+filename.rstrip('.fa')
-			sys.exit()
+		# #Killing the program if it doesn't make the file.
+		# if os.path.exists(sys.argv[2]+'/'+filename.rstrip('.fa')+'_blastn_results.txt')==False:
+		# 	print 'There was a problem making the blastn results file for '+filename.rstrip('.fa')
+		# 	sys.exit()
